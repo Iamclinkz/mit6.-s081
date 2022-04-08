fd58c820067d8855b4f4731bcb6d399f4cd70226
你的githubsun_liyuan <807071409@qq.com>
2022-04-08 17:25:17 +0800

    lab7.2

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,9 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+
+pthread_mutex_t locks[NBUCKET];   //锁
+
 struct entry {
   int key;
   int value;
@@ -40,6 +43,8 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&locks[i]);
+  
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,6 +58,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -60,12 +67,12 @@ get(int key)
 {
   int i = key % NBUCKET;
 
-
+  pthread_mutex_lock(&locks[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&locks[i]);
   return e;
 }
 
@@ -99,6 +106,9 @@ get_thread(void *xa)
 int
 main(int argc, char *argv[])
 {
+  for(int i=0;i!=NBUCKET;i++)
+    pthread_mutex_init(&locks[i], NULL);
+
   pthread_t *tha;
   void *value;
   double t1, t0;
